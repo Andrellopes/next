ca5c0694db7573da952274d5bc1e402caa92ddfc
allc <andrellopes@live.com>
2021-05-10 20:58:09 -0300

    email

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,38 +2,37 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 export default (req, res) => {
+    if (req.method === 'POST') {
+        const {
+            name,
+            email,
+            text
+        } = req.body;
 
-    const {
-        name,
-        email,
-        text
-    } = req.body;
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD,
+            },
+        });
 
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASSWORD,
-        },
-    });
+        const mailOption = {
+            from: `${email}`,
+            to: `${process.env.EMAIL}`,
+            subject: `New mail from ${email}`,
+            text: `
+        ${name} wrote:
+        ${text}
+        `,
+        };
 
-    const mailOption = {
-        from: `${email}`,
-        to: `${process.env.EMAIL}`,
-        subject: `New mail from ${email}`,
-        text: `
-    ${name} wrote:
-    ${text}
-    `,
-    };
-
-    transporter.sendMail(mailOption, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.send("error" + JSON.stringify(err));
-        } else {
-            console.log("mail send");
-            res.send("success");
-        }
-    });
+        transporter.sendMail(mailOption, (err, data) => {
+            if (err) {
+                res.status(400).send("error" + JSON.stringify(err));
+            } else {
+                res.status(200).send("success");
+            }
+        });
+    }
 }; 
